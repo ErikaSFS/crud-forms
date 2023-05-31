a4ff1fb5497ff870b3939320e3bd6e718320b857
Erika Santos <dossantos.erika08@gmail.com>
2023-05-31 14:28:01 -0300

    Add Component Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public formAutentificacao: FormGroup = new FormGroup ('');
+
+
+  public ngOnInit(): void {
+    //this.createForm();
+  }
 
 }
